c0f3c0dcdebfa3c27d131027e75d693b8e23cd70
mithun <mithun@ironhorseserv.com>
2024-08-13 15:52:53 -0500

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -17,85 +17,85 @@ const config = {
 
 export const app = initializeApp(config);
 export const auth = getAuth(app);
-export const messaging = getMessaging(app);
+// export const messaging = getMessaging(app);
 
 
-export const requestForToken = () => {
-    return getToken(messaging, { vapidKey: process.env.REACT_APP_FIREBASE_VAP_ID_KEY})
-        .then((currentToken) => {
-            if (currentToken) {
-                console.log('current token for client: ', currentToken);
-                if(JSON.parse(localStorage.getItem(process.env.REACT_APP_USER_TOKEN_LOCAL_STORAGE))["id"]){
-                    return
-                }
+// export const requestForToken = () => {
+//     return getToken(messaging, { vapidKey: process.env.REACT_APP_FIREBASE_VAP_ID_KEY})
+//         .then((currentToken) => {
+//             if (currentToken) {
+//                 console.log('current token for client: ', currentToken);
+//                 if(JSON.parse(localStorage.getItem(process.env.REACT_APP_USER_TOKEN_LOCAL_STORAGE))["id"]){
+//                     return
+//                 }
+//
+//                 let data = qs.stringify({
+//                     'user_id': JSON.parse(localStorage.getItem(process.env.REACT_APP_USER_TOKEN_LOCAL_STORAGE))["id"],
+//                     'token': currentToken
+//                 });
+//                 let config = {
+//                     method: 'post',
+//                     url: process.env.REACT_APP_BIRCH_API_URL + 'update_token',
+//                     headers: {
+//                         'Content-Type': 'application/x-www-form-urlencoded',
+//                         'Accept': 'application/json'
+//                     },
+//                     data: data
+//                 };
+//
+//                 axios.request(config)
+//                     .then((response) => {
+//                         console.log(response.status)
+//                         if (response.status === 200) {
+//                         } else {
+//                             console.log("something wrong")
+//                         }
+//
+//                     })
+//                     .catch((error) => {
+//                         console.log(error);
+//                     });
+//                 // ...
+//             } else {
+//                 console.log('No registration token available. Request permission to generate one.');
+//                 // Show permission UI.
+//             }
+//         })
+//         .catch((err) => {
+//             console.log('An error occurred while retrieving token. ', err);
+//             // ...
+//         });
+// };
+//
+// export const onMessageListener = () =>
+//     new Promise((resolve) => {
+//         onMessage(messaging, (payload) => {
+//             resolve(payload);
+//         });
+//     });
 
-                let data = qs.stringify({
-                    'user_id': JSON.parse(localStorage.getItem(process.env.REACT_APP_USER_TOKEN_LOCAL_STORAGE))["id"],
-                    'token': currentToken
-                });
-                let config = {
-                    method: 'post',
-                    url: process.env.REACT_APP_BIRCH_API_URL + 'update_token',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Accept': 'application/json'
-                    },
-                    data: data
-                };
-
-                axios.request(config)
-                    .then((response) => {
-                        console.log(response.status)
-                        if (response.status === 200) {
-                        } else {
-                            console.log("something wrong")
-                        }
-
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    });
-                // ...
-            } else {
-                console.log('No registration token available. Request permission to generate one.');
-                // Show permission UI.
-            }
-        })
-        .catch((err) => {
-            console.log('An error occurred while retrieving token. ', err);
-            // ...
-        });
-};
-
-export const onMessageListener = () =>
-    new Promise((resolve) => {
-        onMessage(messaging, (payload) => {
-            resolve(payload);
-        });
-    });
-
-const setupNotifications = async () => {
-    try {
-        // Request permission for notifications
-        const permission = await Notification.requestPermission();
-
-        if (permission === 'granted') {
-            console.log('Notification permission granted.');
-            // Get the FCM token
-            const token = await getToken(messaging);
-            console.log('FCM Token:', token);
-        } else {
-            console.log('Notification permission denied.');
-        }
-        // Handle foreground notifications
-        onMessage(messaging, (payload) => {
-            console.log('Foreground Message:', payload);
-            // Handle the notification or update your UI
-        });
-    } catch (error) {
-        console.error('Error setting up notifications:', error);
-    }
-};
+// const setupNotifications = async () => {
+//     try {
+//         // Request permission for notifications
+//         const permission = await Notification.requestPermission();
+//
+//         if (permission === 'granted') {
+//             console.log('Notification permission granted.');
+//             // Get the FCM token
+//             const token = await getToken(messaging);
+//             console.log('FCM Token:', token);
+//         } else {
+//             console.log('Notification permission denied.');
+//         }
+//         // Handle foreground notifications
+//         onMessage(messaging, (payload) => {
+//             console.log('Foreground Message:', payload);
+//             // Handle the notification or update your UI
+//         });
+//     } catch (error) {
+//         console.error('Error setting up notifications:', error);
+//     }
+// };
 
 export const provider = new GoogleAuthProvider();
 provider.addScope("email")

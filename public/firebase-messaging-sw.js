10d78f53e1731d04c56b717b5e0c047302c927ef
mithun <mithun@ironhorseserv.com>
2024-09-18 09:39:41 -0500

    push added

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,3 +4,38 @@
  * @created : 9/18/2024, Wednesday
  * Description:
  **/
+
+// eslint-disable-next-line no-undef
+import firebase from "firebase/compat";
+
+// eslint-disable-next-line no-undef
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+// eslint-disable-next-line no-undef
+importScripts(
+    "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
+
+// Initialize the Firebase app in the service worker
+// "Default" Firebase configuration (prevents errors)
+const defaultConfig = {
+    apiKey: true,
+    projectId: true,
+    messagingSenderId: true,
+    appId: true,
+};
+
+firebase.initializeApp(defaultConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.image,
+    };
+
+    // eslint-disable-next-line no-restricted-globals
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});

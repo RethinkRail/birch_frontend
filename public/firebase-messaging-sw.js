eafc1475d170d108c935da8745d0e5688b13dc44
mithun <mithun@ironhorseserv.com>
2024-09-18 14:38:43 -0500

    bg push

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
+importScripts('https://www.gstatic.com/firebasejs/9.x/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.x/firebase-messaging.js')
 
 
 
@@ -21,3 +21,15 @@ const firebaseApp = initializeApp({
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = getMessaging(firebaseApp);
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message:', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/firebase-logo.png',
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});

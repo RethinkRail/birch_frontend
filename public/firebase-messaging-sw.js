4f6c96420811fb8c08d94356fe412f1beaf2f5e9
mithun <mithun@ironhorseserv.com>
2024-09-18 14:55:04 -0500

    bg push

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,17 +1,9 @@
-import {initializeApp} from "firebase/app";
+// Import Firebase scripts using importScripts() in service workers
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts(
-    "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
-);
-
-
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp({
-
-
+// Initialize Firebase
+firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -21,9 +13,8 @@ const firebaseApp = initializeApp({
     measurementId: process.env.REACT_APP_FIREBASE_MESSUREMENT_ID,
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = getMessaging(firebaseApp);
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
     console.log('[firebase-messaging-sw.js] Received background message:', payload);

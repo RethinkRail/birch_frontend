61641c2da932b40019e6b8315fc1f31a52096a29
mithun <mithun@ironhorseserv.com>
2025-05-02 13:42:19 -0500

    updated

--- src/utils/aarHelper.js
+++ src/utils/aarHelper.js
@@ -15,6 +15,7 @@ import {round2Dec} from "./NumberHelper";
  */
 
 export function printAAR(item, _wheel_detail = false, forWhom) {
+    console.log(item)
     let data = null;
     let wheel_detail = _wheel_detail;
     let record_format = {

766d4b72fb621bd48a48b09c8c8fc81ce4e926fc
Mithun Sarker <mithun@ihrail.com>
2025-08-15 16:51:37 -0500

    staging updated

--- restart.sh
+++ restart.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-docker kill birch_frontend
-docker stop birch_frontend
-docker rm birch_frontend
-docker build -t birch_frontend .
+docker kill birch_frontend_staging
+docker stop birch_frontend_staging
+docker rm birch_frontend_staging
+docker build -t birch_frontend_staging .
 #docker run -d --name birch_backend -p 8090:8090 birch_backend:latest
 #docker run -d -p 8090:8090 birch_backend:latest
-docker run -d --name birch_frontend -p 3000:80 birch_frontend:latest
-docker logs birch_frontend >& birch_frontend.log
+docker run -d --name birch_frontend_staging -p 5001:5001 birch_frontend_staging:latest
+docker logs birch_frontend_staging >& birch_frontend_staging.log

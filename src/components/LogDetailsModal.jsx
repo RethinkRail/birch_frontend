e6e98e6f4f87b40b9dc49471485c43be5e858433
mithu <mithun@ihrail.com>
2025-11-04 16:34:24 -0600

    day end reporting added

--- src/components/LogDetailsModal.jsx
+++ src/components/LogDetailsModal.jsx
@@ -11,7 +11,15 @@ import React from 'react';
 import {round2Dec} from "../utils/NumberHelper";
 
 const LogDetailsModal = ({ log, onClose, onApprove,onUnApprove,onDelete, onEditClick }) => {
+    console.log(log);
     if (!log) return null;
+
+    function concatEndShiftNotes(logData) {
+        return logData.logs
+            .map(log => log.end_shift_note)      // extract end_shift_note// remove null/undefined
+            .join(' ');                          // join with space (or any separator)
+    }
+
     function formatNotes(notes) {
         if (!notes) return '';
 
@@ -116,6 +124,10 @@ const LogDetailsModal = ({ log, onClose, onApprove,onUnApprove,onDelete, onEditC
                         ))}
                         </tbody>
                     </table>
+                </div>
+                <div className="p-4 bg-gray-50 rounded-md border border-gray-200">
+                    <span className="font-semibold text-gray-700">Employee Notes:</span>
+                    <span className="ml-2 text-gray-600">{concatEndShiftNotes(log)}</span>
                 </div>
 
                 {/* Footer */}
